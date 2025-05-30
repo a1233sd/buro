83e55f1776b421776ed6fabeff0b37f481aed5cb
vladimir <75137066+a1233sd@users.noreply.github.com>
2025-05-30 19:00:31 +0300

    add

--- solve.py
+++ solve.py
@@ -1,32 +1,10 @@
-# Задача 2: минимальная подпоследовательность, содержащая алфавит
-from collections import defaultdict
+# Задача 3: Рекурсивная последовательность
+f = [1, 3]
+a = [1, 3]
+while len(a) <= 40:
+    next_val = 5 * f[-1] + f[-2]
+    f.append(next_val)
+    if next_val % 2 == 1:
+        a.append(next_val)
 
-alphabet = set(range(1, 27))
-with open("data_prog_contest_problem_2.txt") as f:
-    lines = f.readlines()
-    sequence = list(map(int, lines[1].strip().split()))
-
-left = 0
-min_len = float('inf')
-window = None
-count = defaultdict(int)
-found = set()
-
-for right in range(len(sequence)):
-    val = sequence[right]
-    if val in alphabet:
-        count[val] += 1
-        found.add(val)
-
-    while alphabet.issubset(found):
-        if right - left + 1 < min_len:
-            min_len = right - left + 1
-            window = (left, right)
-        left_val = sequence[left]
-        if left_val in alphabet:
-            count[left_val] -= 1
-            if count[left_val] == 0:
-                found.remove(left_val)
-        left += 1
-
-print("Длина минимальной подпоследовательности:", min_len if window else "NONE")
+print("A[39] =", a[39])
